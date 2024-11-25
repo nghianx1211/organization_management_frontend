2bd1614705dee547197d54bd7fd07ee6e0773e9a
nghianx1211 <nghianx1211@gmail.com>
2024-11-25 16:15:58 +0700

    update action

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `/api/employee`
+const BASE_URL = `http://18.206.151.196/api/employee`
 class EmployeeService{
 
     //**Method to get all employee from our api or database */
@@ -23,7 +23,7 @@ class EmployeeService{
 
     /** Method to upload a file */
     uploadFile(formData) {
-        return axios.post(`/api/files`, formData);
+        return axios.post(`http://18.206.151.196/api/files`, formData);
     }
 
 
