d9ac5ccb470e6be74af85ded9c547d254a33685c
nghianx1211 <nghianx1211@gmail.com>
2024-11-27 09:56:40 +0700

    update action use runner

--- src/component/ListEmployeeComponent.js
+++ src/component/ListEmployeeComponent.js
@@ -26,7 +26,7 @@ const ListEmployeeComponent = () => {
         <div className='container'>
             <Link to={"/add-employee"} className='btn btn-primary mb-2 mt-3' href="">Add Employee</Link>
             <FileUpload/>
-            <h2 className='text-center mb-4'>List Employee</h2>
+            <h2 className='text-center mb-4'>List Employee 2</h2>
             <table className='table table-bordered table striped'>
                 <thead>
                     <th>Employee ID</th>
